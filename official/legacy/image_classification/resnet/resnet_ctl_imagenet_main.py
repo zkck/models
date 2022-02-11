a37e78c078d8c64a763753b9688a08cb56e9df4a
Zak Cook <zakcook4@gmail.com>
2022-02-11 12:21:29 +0100

    Add enable_op_determinism

--- official/legacy/image_classification/resnet/resnet_ctl_imagenet_main.py
+++ official/legacy/image_classification/resnet/resnet_ctl_imagenet_main.py
@@ -100,6 +100,10 @@ def run(flags_obj):
   keras_utils.set_session_config()
   performance.set_mixed_precision_policy(flags_core.get_tf_dtype(flags_obj))
 
+  # zkck: Enable op determinism
+  tf.keras.utils.set_random_seed(1)
+  tf.config.experimental.enable_op_determinism()
+
   if tf.config.list_physical_devices('GPU'):
     if flags_obj.tf_gpu_thread_mode:
       keras_utils.set_gpu_thread_mode_and_count(
