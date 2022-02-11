8a1d4c068d419473aa85d274504419f5b2f0e484
Zak Cook <zakcook4@gmail.com>
2022-02-11 17:19:03 +0100

    Keyword argument after positional argument

--- official/legacy/image_classification/resnet/imagenet_preprocessing.py
+++ official/legacy/image_classification/resnet/imagenet_preprocessing.py
@@ -381,13 +381,13 @@ def _decode_crop_and_flip(image_buffer, bbox, num_channels):
   # bounding box. If no box is supplied, then we assume the bounding box is
   # the entire image.
   sample_distorted_bounding_box = tf.image.sample_distorted_bounding_box(
-      seed=1,
       tf.image.extract_jpeg_shape(image_buffer),
       bounding_boxes=bbox,
       min_object_covered=0.1,
       aspect_ratio_range=[0.75, 1.33],
       area_range=[0.05, 1.0],
       max_attempts=100,
+      seed=1,
       use_image_if_no_bounding_boxes=True)
   bbox_begin, bbox_size, _ = sample_distorted_bounding_box
 
