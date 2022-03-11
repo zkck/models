bc1be7ae0208fe808782f661266e678951dbf360
Zak Cook <zakcook4@gmail.com>
2022-03-11 14:06:49 +0100

    Loop for tensorboard

--- benchmark.sh
+++ benchmark.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 cd official/legacy/image_classification/resnet
 
-python3 resnet_ctl_imagenet_main.py \
-        --tpu=$TPU_NAME \
-        --model_dir=$MODEL_DIR \
-        --data_dir=$DATA_DIR \
-        --batch_size=1024 \
-        --steps_per_loop=500 \
-        --train_epochs=20 \
-        --use_synthetic_data=false \
-        --dtype=fp32 \
-        --enable_eager=true \
-        --enable_tensorboard=true \
-        --distribution_strategy=tpu \
-        --log_steps=50 \
-        --single_l2_loss_op=true \
-        --use_tf_function=true \
+for i in {1..10}
+do
+	python3 resnet_ctl_imagenet_main.py \
+		--tpu=$TPU_NAME \
+		--model_dir=$MODEL_DIR \
+		--data_dir=$DATA_DIR/run$i \
+		--batch_size=1024 \
+		--steps_per_loop=500 \
+		--train_epochs=20 \
+		--use_synthetic_data=false \
+		--dtype=fp32 \
+		--enable_eager=true \
+		--enable_tensorboard=true \
+		--distribution_strategy=tpu \
+		--log_steps=50 \
+		--single_l2_loss_op=true \
+		--use_tf_function=true
+done
