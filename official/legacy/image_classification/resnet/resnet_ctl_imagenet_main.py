a51c1c7f71c38f8d22c4127372a42437444b0be5
Zak Cook <zakcook4@gmail.com>
2022-04-10 12:10:31 +0200

    Move enable_op_determinism earlier

--- official/legacy/image_classification/resnet/resnet_ctl_imagenet_main.py
+++ official/legacy/image_classification/resnet/resnet_ctl_imagenet_main.py
@@ -102,11 +102,6 @@ def run(flags_obj):
   keras_utils.set_session_config()
   performance.set_mixed_precision_policy(flags_core.get_tf_dtype(flags_obj))
 
-  if flags_obj.deterministic:
-    logging.info("Enabled op determinism")
-    tf.keras.utils.set_random_seed(1)
-    tf.config.experimental.enable_op_determinism()
-
   if tf.config.list_physical_devices('GPU'):
     if flags_obj.tf_gpu_thread_mode:
       keras_utils.set_gpu_thread_mode_and_count(
@@ -191,6 +186,11 @@ def run(flags_obj):
 
 
 def main(_):
+  if flags.FLAGS.deterministic:
+    tf.keras.utils.set_random_seed(1)
+    tf.config.experimental.enable_op_determinism()
+    logging.info("Enabled op determinism")
+
   model_helpers.apply_clean(flags.FLAGS)
   stats = run(flags.FLAGS)
   logging.info('Run stats:\n%s', stats)
