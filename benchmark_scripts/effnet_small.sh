2093f0eb0919c82f6c5da1aa25e86b3b71639902
Zak Cook <zakcook4@gmail.com>
2022-05-05 11:07:58 +0200

    Propagate args to effnet benchmark

--- benchmark_scripts/effnet_small.sh
+++ benchmark_scripts/effnet_small.sh
@@ -9,4 +9,5 @@ python3 classifier_trainer.py \
         --tpu=$TPU_NAME \
         --model_dir=$MODEL_DIR \
         --data_dir=$DATA_DIR/imagenet2012/tfrecords \
-        --config_file=configs/efficientnet-b0-tpu.yaml
+        --config_file=configs/efficientnet-b0-tpu.yaml \
+        "$@"
