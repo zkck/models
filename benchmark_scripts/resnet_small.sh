9a5798f6d988a7714b4aa5ac53512d7df9f031df
Zak Cook <zakcook4@gmail.com>
2022-05-05 16:34:44 +0200

    Add resnet config

---
+++ benchmark_scripts/resnet_small.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd official/legacy/image_classification
+
+bash python3 classifier_trainer.py \
+        --mode=train_and_eval \
+        --model_type=resnet \
+        --dataset=imagenet \
+        --tpu=$TPU_NAME \
+        --model_dir=$MODEL_DIR \
+        --data_dir=$DATA_DIR \
+        --config_file=configs/tpu.yaml \
+        "$@"
