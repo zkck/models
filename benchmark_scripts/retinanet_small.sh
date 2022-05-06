dfef29fcc00dcc0ca7b9ad6fd88f7bb6032866b0
Zak Cook <zakcook4@gmail.com>
2022-05-06 16:40:52 +0200

    Configure with correct (?) yaml

--- benchmark_scripts/retinanet_small.sh
+++ benchmark_scripts/retinanet_small.sh
@@ -4,11 +4,24 @@ TRAIN_FILE_PATTERN="${DATA_DIR?}/coco/train-*-of-*.tfrecord"
 EVAL_FILE_PATTERN="${DATA_DIR?}/coco/val-*-of-*.tfrecord"
 VAL_JSON_FILE="${DATA_DIR?}/coco/raw-data/annotations/instances_val2017.json"
 
+PARAMS="
+type: retinanet
+architecture:
+  backbone: spinenet
+  multilevel_features: identity
+spinenet:
+  model_id: 49
+train_file_pattern: ${TRAIN_FILE_PATTERN?}
+eval:
+  val_json_file: ${VAL_JSON_FILE?}
+  eval_file_pattern: ${EVAL_FILE_PATTERN?}
+"
+
 cd ~/models/official/legacy/detection
 python3 main.py \
   --strategy_type=tpu \
   --tpu="${TPU_NAME?}" \
   --model_dir="${MODEL_DIR?}" \
   --mode=train \
-  --params_override="{ type: retinanet, architecture: {backbone: spinenet, multilevel_features: identity}, spinenet: {model_id: 49}, train_file_pattern: ${TRAIN_FILE_PATTERN?} }, eval: { val_json_file: ${VAL_JSON_FILE?}, eval_file_pattern: ${EVAL_FILE_PATTERN?} } }" \
+  --params_override="${PARAMS?}" \
   "$@"
