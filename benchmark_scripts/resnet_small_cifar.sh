f9f6c51278b4ceac39ea68131f94341cccc971ba
Zak Cook <zakcook4@gmail.com>
2022-05-09 17:19:25 +0200

    Add CIFAR resnet benchmark

---
+++ benchmark_scripts/resnet_small_cifar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd official/legacy/image_classification
+
+python3 classifier_trainer.py \
+        --mode=train_and_eval \
+        --model_type=resnet \
+        --dataset=imagenet \
+        --tpu=$TPU_NAME \
+        --model_dir=$MODEL_DIR \
+        --data_dir=$DATA_DIR/cifar10/3.0.2/ \
+        --config_file=configs/tpu_cifar.yaml \
+        "$@"
