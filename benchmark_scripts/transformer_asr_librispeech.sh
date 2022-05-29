8ff0c480b66cdc7ae77864e8a2068e4d081b8d03
Zak Cook <zakcook4@gmail.com>
2022-05-29 17:00:44 +0200

    Use ~ instead of $HOME

--- benchmark_scripts/transformer_asr_librispeech.sh
+++ benchmark_scripts/transformer_asr_librispeech.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd official/legacy/speech
-python transformer_asr.py --dataset_name=librispeech --data_dir="$HOME/LibriSpeech/dev-clean" --max_target_len=2048 --model_dir="${MODEL_DIR?}" "$@"
+python transformer_asr.py --dataset_name=librispeech --data_dir="~/LibriSpeech/dev-clean" --max_target_len=2048 --model_dir="${MODEL_DIR?}" "$@"
