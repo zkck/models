d08f0d9e4a62145da15ecab0294e1f0a02c29257
Zak Cook <zakcook4@gmail.com>
2022-06-07 15:19:34 +0200

    Use different main script to get stats

--- benchmark_scripts/resnet_variance.sh
+++ benchmark_scripts/resnet_variance.sh
@@ -2,30 +2,17 @@
 
 # DATA_DIR is the inherited by top-level script,
 # it's the root directory of the test data
-IMAGENET_DIR=$DATA_DIR/imagenet2012/tfrecords
-
-TRAIN_EPOCHS=90
-
-args=(
-    "--tpu=$TPU_NAME"
-    "--data_dir=$IMAGENET_DIR"
-    "--batch_size=1024"
-    "--steps_per_loop=500"
-    "--train_epochs=$TRAIN_EPOCHS"
-    "--use_synthetic_data=false"
-    "--dtype=fp32"
-    "--enable_eager=true"
-    "--enable_tensorboard=true"
-    "--distribution_strategy=tpu"
-    "--log_steps=50"
-    "--single_l2_loss_op=true"
-    "--use_tf_function=true"
-)
-echo "Using ${args[@]}"
-
-cd official/legacy/image_classification/resnet
+cd official/legacy/image_classification
 
 for i in {1..5}
 do
-    python3 resnet_ctl_imagenet_main.py "${args[@]}" --model_dir=$MODEL_DIR/run$i
+    python3 classifier_trainer.py \
+            --mode=train_and_eval \
+            --model_type=resnet \
+            --dataset=imagenet \
+            --tpu=$TPU_NAME \
+            --model_dir="$MODEL_DIR/run$i" \
+            --data_dir=$DATA_DIR/imagenet2012/tfrecords \
+            --config_file=configs/tpu.yaml \
+            "$@"
 done
