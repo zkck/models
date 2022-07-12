b96f3133bc07f255104cdf5bbc36a3a9d4511829
Zak Cook <zakcook4@gmail.com>
2022-07-12 15:15:31 +0200

    Do not reshuffle each iteration

--- official/legacy/image_classification/resnet/imagenet_preprocessing.py
+++ official/legacy/image_classification/resnet/imagenet_preprocessing.py
@@ -113,7 +113,7 @@ def process_record_dataset(dataset,
 
   if is_training:
     # Shuffles records before repeating to respect epoch boundaries.
-    dataset = dataset.shuffle(buffer_size=shuffle_buffer, seed=22)
+    dataset = dataset.shuffle(buffer_size=shuffle_buffer, seed=22, reshuffle_each_iteration=False)
     # Repeats the dataset for the number of epochs to train.
     # dataset = dataset.repeat()
 
