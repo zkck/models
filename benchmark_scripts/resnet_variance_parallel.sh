3da4596edbe55727e35a4a0df9ace061dbeb932f
Zak Cook <zakcook4@gmail.com>
2022-07-13 20:03:50 +0200

    Add resnet parallel variance script

---
+++ benchmark_scripts/resnet_variance_parallel.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export ZCK_PARALLEL_RANDOMNESS=1
+
+# DATA_DIR is the inherited by top-level script,
+# it's the root directory of the test data
+cd official/legacy/image_classification
+
+
+
+for i in {1..5}
+do
+    python3 classifier_trainer.py \
+            --mode=train_and_eval \
+            --model_type=resnet \
+            --dataset=imagenet \
+            --tpu=$TPU_NAME \
+            --model_dir="$MODEL_DIR/run$i" \
+            --data_dir=$DATA_DIR/imagenet2012/tfrecords \
+            --config_file=configs/tpu.yaml \
+            --seed=1 \
+            --enable_op_determinism
+done
