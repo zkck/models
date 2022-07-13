542bb9493a17f061795892ac250305a250527e1b
Zak Cook <zakcook4@gmail.com>
2022-07-13 11:09:43 +0200

    Make determinstic version of HeNormal

--- official/benchmark/models/resnet_cifar_model.py
+++ official/benchmark/models/resnet_cifar_model.py
@@ -32,10 +32,44 @@ BATCH_NORM_EPSILON = 1e-5
 L2_WEIGHT_DECAY = 2e-4
 
 
+
+class HeNormal(tf.keras.initializers.VarianceScaling):
+  """He normal initializer.
+   Also available via the shortcut function
+  `tf.keras.initializers.he_normal`.
+  It draws samples from a truncated normal distribution centered on 0 with
+  `stddev = sqrt(2 / fan_in)` where `fan_in` is the number of input units in the
+  weight tensor.
+  Examples:
+  >>> # Standalone usage:
+  >>> initializer = tf.keras.initializers.HeNormal()
+  >>> values = initializer(shape=(2, 2))
+  >>> # Usage in a Keras layer:
+  >>> initializer = tf.keras.initializers.HeNormal()
+  >>> layer = tf.keras.layers.Dense(3, kernel_initializer=initializer)
+  Args:
+    seed: A Python integer. Used to make the behavior of the initializer
+      deterministic. Note that a seeded
+      initializer will not produce the same random values across multiple calls,
+      but multiple initializers will produce the same sequence when constructed
+      with the same seed value.
+  References:
+    - [He et al., 2015](https://arxiv.org/abs/1502.01852)
+  """
+
+  def __init__(self, seed=None):
+    super(HeNormal, self).__init__(
+        scale=2., mode='fan_in', distribution='truncated_normal', seed=seed)
+    self._random_generator._force_generator = True
+
+  def get_config(self):
+    return {'seed': self.seed}
+
+
 class DeterministicInitializerFactory:
 
   _INITIALIZERS = {
-    'he_normal': tf.keras.initializers.HeNormal
+    'he_normal': HeNormal
   }
 
   def __init__(self, seed) -> None:
