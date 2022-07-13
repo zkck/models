c8cc51c93af74220782a4b6cce438f22ce0d25fc
Zak Cook <zakcook4@gmail.com>
2022-07-14 00:09:42 +0200

    Adapt script to benchmark/models

--- benchmark_scripts/resnet_variance.sh
+++ benchmark_scripts/resnet_variance.sh
@@ -2,18 +2,17 @@
 
 # DATA_DIR is the inherited by top-level script,
 # it's the root directory of the test data
-cd official/legacy/image_classification
+cd official/benchmark/models
 
 for i in {1..5}
 do
-    python3 classifier_trainer.py \
-            --mode=train_and_eval \
-            --model_type=resnet \
-            --dataset=imagenet \
-            --tpu=$TPU_NAME \
-            --model_dir="$MODEL_DIR/run$i" \
-            --data_dir=$DATA_DIR/imagenet2012/tfrecords \
-            --config_file=configs/tpu.yaml \
-            --seed=1 \
-            --enable_op_determinism
+	python resnet_cifar_main.py \
+		--data_dir /home/zkck/training-data/imagenet2012/tfrecords \
+		--tpu "local" \
+		-ds tpu \
+		--train_epochs 90 \
+		--enable_tensorboard \
+		--model_dir "/home/zkck/data/run$i" \
+		--seed 1 \
+		--enable_op_determinism
 done
