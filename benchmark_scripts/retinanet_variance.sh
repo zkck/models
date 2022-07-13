902435387435243165bd1b5fb13d5a8eab888ede
Zak Cook <zakcook4@gmail.com>
2022-07-13 16:50:31 +0200

    Add retinanet variance script

---
+++ benchmark_scripts/retinanet_variance.sh
@@ -0,0 +1,42 @@
+
+#!/bin/bash
+
+TRAIN_FILE_PATTERN="${DATA_DIR?}/coco/train-*-of-*.tfrecord"
+EVAL_FILE_PATTERN="${DATA_DIR?}/coco/val-*-of-*.tfrecord"
+VAL_JSON_FILE="${DATA_DIR?}/coco/raw-data/annotations/instances_val2017.json"
+
+PARAMS="
+type: retinanet
+architecture:
+  backbone: spinenet
+  multilevel_features: identity
+spinenet:
+  model_id: 49
+train:
+  total_steps: 1000
+  train_file_pattern: ${TRAIN_FILE_PATTERN?}
+eval:
+  val_json_file: ${VAL_JSON_FILE?}
+  eval_file_pattern: ${EVAL_FILE_PATTERN?}
+"
+
+cd ~/models/official/legacy/detection
+
+for i in {1..5}
+do
+  model_dir="${MODEL_DIR?}/run$i"
+  python3 main.py \
+    --strategy_type=tpu \
+    --tpu="${TPU_NAME?}" \
+    --model_dir="$model_dir" \
+    --mode=train \
+    --params_override="${PARAMS?}"
+  # Remove checkpoints
+  python3 main.py \
+    --strategy_type=tpu \
+    --tpu="${TPU_NAME?}" \
+    --model_dir="$model_dir" \
+    --checkpoint_path=$model_dir \
+    --mode=eval_once \
+    --params_override="${PARAMS?}"
+done
