3cf8ad700a3438c75bbb04faa3656889335b17c2
Zak Cook <zakcook4@gmail.com>
2022-07-25 11:21:50 +0200

    Remove specaugment

--- official/legacy/speech/dataset.py
+++ official/legacy/speech/dataset.py
@@ -83,7 +83,7 @@ class DatasetFactory(abc.ABC):
         audio_ds = self.create_audio_ds(data)
         text_ds = self.create_text_ds(data)
         ds = tf.data.Dataset.zip((audio_ds, text_ds))
-        ds = ds.map(lambda x, y: {"source": specaugment.spec_augment(x), "target": y}, num_parallel_calls=tf.data.AUTOTUNE)
+        ds = ds.map(lambda x, y: {"source": x, "target": y}, num_parallel_calls=tf.data.AUTOTUNE)
         ds = ds.batch(bs, drop_remainder=True)
         ds = ds.prefetch(tf.data.AUTOTUNE)
         return ds
